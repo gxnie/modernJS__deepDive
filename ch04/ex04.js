b22d0f1255915ace98a89a1090c28e78fd4e2ab0
gxnie <wlsdl7520@naver.com>
2025-03-11 17:11:08 +0900

    Add ch4. Variable

---
+++ ch04/ex04.js
@@ -0,0 +1,20 @@
+// 예제를 직접 쳐보면서 이해하기
+var userId = 1;
+var userName = "Lee";
+
+var user = { id: 1, name: 'Lee' };
+
+var users = [
+    { id: 1, name: 'Lee'},
+    { id: 2, name: 'Kim'}
+];
+
+var result = 10 + 20; // 30
+
+// 값의 할당
+console.log(score); // undefined
+
+score = 80; // 값의 할당
+var score; // 변수 선언
+
+console.log(score); 
